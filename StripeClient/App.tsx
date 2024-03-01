20df83ca7f9590b9f57f2ec5707469990951eb0b
supersquad <supersquad@supersquadui-MacBookPro.local>
2024-03-01 16:02:18 +0900

    feat: stripe example

---
+++ StripeClient/App.tsx
@@ -0,0 +1,131 @@
+import { Button, StyleSheet, View } from 'react-native'
+import React, { useState } from 'react'
+import { CardField, StripeProvider, createPaymentMethod } from '@stripe/stripe-react-native';
+import axios from 'axios';
+
+const stripePublishableKey = "";
+
+const App = () => {
+  const [customerId, setCustomerId] = useState<string>('');
+  const [paymentMethodId, setPaymentMethodId] = useState<string>('');
+  const [paymentIntentId, setPaymentIntentId] = useState<string>('')
+
+
+  // PaymentIntent 생성 API 및 컨펌 API 호출
+  const handlePayment = async () => {
+    // PaymentIntnet 생성 @params customerId 
+    const res = await axios.post('http://localhost:8080/create-payment-intent', {
+      id: customerId, // customerId는 기본적으로 클라이언트에서 가지고 있어야 함
+      amount: 1000
+    });
+
+    console.log("createRes", res.data)
+    setPaymentIntentId(res.data.paymentIntent.id)
+
+    // PaymentIntent 컨펌 @params paymentIntentId, paymentMethodId
+    const confirmRes = await axios.post('http://localhost:8080/confirm-payment-intent', {
+      paymentIntentId: res.data.paymentIntent.id,
+      paymentMethod: paymentMethodId 
+      
+      // 서버 아래 코드에서 paymentMethod 가져올 예정
+      // app.post('/retrieve-payment-method', async(req, res) => {})
+
+    })
+    console.log('confirmRes', confirmRes.data)
+  }
+
+    // customer 생성 API 호출
+  const createCustomer = async() => {
+    const response = await axios.post('http://localhost:8080/create-customer', {
+      body: {
+        email: 'hassanpumped17@gmail.com'
+      }
+    })
+
+    setCustomerId(response.data.customer.id);
+    console.log(response.data.customer)
+  }
+
+  // customer 존재하는지 조회
+  const fetchSingleCustomer = async() => {
+    const response = await  axios.post('http://localhost:8080/fetch-customer', {
+      id: customerId // firebase나 클라이언트 스토리지에서 관리
+    })
+
+    console.log(response.data)
+  }
+
+  // 
+  const fetchPaymentMethod = async() => {
+    const response = await axios.post('http://localhost:8080/retrieve-payment-method', {
+      id: customerId // firebase나 클라이언트 스토리지에서 관리
+    })
+
+    console.log("paymentMethod", response.data)
+  }
+
+  // Stripe API 사용해서 클라이언트에서 사용.
+  const handleCreatePaymentMethod = async() => {
+    const paymentMethod = await createPaymentMethod({
+      paymentMethodType: 'Card',
+      paymentMethodData: {
+        billingDetails: {
+          email: 'hassanpumped17@gmail.com'
+        }
+      }
+    })
+    setPaymentMethodId(paymentMethod.paymentMethod?.id || '')
+    console.log("paymentMethod", paymentMethod.paymentMethod?.id)
+
+    const response = await axios.post('http://localhost:8080/attach-payment-method', {
+      customer: customerId,
+      paymentMethod: paymentMethod.paymentMethod?.id // 위에 객체에서 얻은 아이디
+    });
+
+    console.log("response", response.data)
+  }
+
+
+  return (
+    <StripeProvider
+      publishableKey={stripePublishableKey}
+      urlScheme='stripeclient://stripe'
+    >
+      <View style={styles.container}>
+        <Button title='createCustomer' onPress={() => createCustomer()}/>
+        <Button title='getSingleCustomer' onPress={() => fetchSingleCustomer()}/>
+        <Button title='retrievePaymentMethod' onPress={() => fetchPaymentMethod()}/>
+        <Button title='createPaymentMethod' onPress={async () => handleCreatePaymentMethod()}/>
+        <CardField
+          autofocus
+          cardStyle={{
+            borderWidth: 1,
+            borderRadius: 20,
+          }}
+          onCardChange={(card) => {
+            console.log("card", card)
+          }}
+          style={{
+            width: '100%',
+            height: 60,
+          }}
+        />
+        <Button title='handlePayment' onPress={() => handlePayment()}/>
+      </View>
+    </StripeProvider>
+
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
+
+
---
+++ StripeServer/index.js
@@ -0,0 +1,201 @@
+import bodyParser from 'body-parser';
+import express from 'express';
+import Stripe from 'stripe';
+
+const stripePublishableKey = "pk_test_51NrXxjFhR7Qa2jLveDu2naP1HobAyHpsqmNxkF3VtM6uSuQN3YmdETfCIv5bk3WP4wOOZersS4gwvP08AQp0rTyq00kHEXCLAL";
+const stripeSecretKey = "sk_test_51NrXxjFhR7Qa2jLvzc0ETGKrF04vL9r0d5oxdPCPexbpqU1Il2hUwc2T3WkWgPgve3LMnpf5xNa7XJ9zYOhxea0300ImQ6t3jO";
+
+const app = express();
+
+app.use((req, res, next) => {
+  bodyParser.json()(req, res, next);
+});
+
+app.get('/stripe-key', (_, res) => {
+  return res.send({publishableKey: stripePublishableKey});
+});
+
+app.post('/create-payment-intent', async (req, res) => {
+  const { id, amount } = req.body;
+
+  const stripe = new Stripe(stripeSecretKey, {
+    apiVersion: '2023-10-16',
+    typescript: true,
+  });
+
+  // 원래 email로 customer 생성하던 것에서 customerId로 customer 조회, 
+  // 서버에서는 따로 customerId 조회하는 API 필요
+  const customer = await stripe.customers.retrieve(id);
+
+  const params = {
+    amount: amount,
+    currency: 'usd',
+    customer: customer.id,
+    payment_method_options: {
+      card: {
+        request_three_d_secure: 'automatic',
+      },
+    },
+    payment_method_types: ['card'],
+  };
+
+  try {
+    const paymentIntent = await stripe.paymentIntents.create(params);
+
+    // 클라이언트에서는 PaymentIntentId만 사용함.
+    return res.send({
+      paymentIntent
+    });
+  } catch (error) {
+    return res.send({
+      error: error.raw.message,
+    });
+  }
+});
+
+// 처음에 결제하면 자동으로 customer 객체가 생성됨.
+// 따로 customer 생성할 수도 있음.
+app.post('/create-customer', async(req, res) => {
+  const { email } = req.body;
+
+  const stripe = new Stripe(stripeSecretKey, {
+    apiVersion: '2023-10-16',
+    typescript: false
+  })
+
+  const customer = await stripe.customers.create({ email });
+
+  console.log(customer)
+
+  return res.json({
+    customer: customer
+  })
+})
+
+// customerId로 customer 조회 가능.
+app.post('/fetch-customer', async(req, res) => {
+  const { id } = req.body;
+
+  const stripe = new Stripe(stripeSecretKey, {
+    apiVersion: '2023-10-16',
+    typescript: false
+  });
+
+  const customer = await stripe.customers.retrieve(id);
+
+  console.log(customer)
+
+  return res.json({
+    customer: customer
+  })
+})
+
+// customerId로 해당 customer가 가진 paymentMethod 조회.
+// 많을 수도 있어서 아래 코드에서는 첫 번째만 사용.
+app.post('/retrieve-payment-method', async(req, res) => {
+  const { id } = req.body;
+
+  console.log(id)
+
+  const stripe = new Stripe(stripeSecretKey, {
+    apiVersion: '2023-10-16',
+    typescript: false,
+  });
+
+  const paymentMethodList = await stripe.customers.listPaymentMethods(id, {
+    limit: 1,
+  })
+
+  console.log(paymentMethodList)
+  console.log("paymentMethod", paymentMethodList.data.length !== 0 && paymentMethodList.data[0].card);
+
+  return res.json({
+    paymentMethodList 
+  })
+})
+
+// 현재 플로우에서도 paymentMethod 객체가 생성되는지는 모르겠음.
+// 피그마에서 따로 카드등록 화면도 있어서 어차피 필요할 듯.
+
+// 아래 코드가 스트라이프에서 메일 받은 코드. 사용 X
+// paymentMethod는 클라이언트에서 등록할 수 있음.
+// app.post('/create-payment-method', async(req, res) => {
+//   const { type, customer } = req.body;
+
+//   const stripe = new Stripe(stripeSecretKey, {
+//     apiVersion: '2023-10-16',
+//     typescript: false,
+//   });
+
+//   console.log(type, customer)
+
+
+//   const paymentMethod = await stripe.paymentMethods.create({
+//     type: 'card',
+//     card: {
+//       number: '4242424242424242',
+//       exp_month: 4,
+//       exp_year: 2024,
+//       cvc: '242',
+//     },
+//   });
+
+//   console.log("paymentMethod", paymentMethod);
+//   return res.json({
+//     paymentMethod
+//   })
+// })
+
+// customerId와 paymentMethod를 받아서 연동
+app.post('/attach-payment-method', async(req, res) => {
+  const { customer, paymentMethod: pid } = req.body;
+
+  console.log(customer, pid)
+
+  const stripe = new Stripe(stripeSecretKey, {
+    apiVersion: '2023-10-16',
+    typescript: false,
+  });
+
+  const paymentMethod = await stripe.paymentMethods.attach(
+    pid,
+    {
+      customer: customer,
+    }
+  );
+
+  console.log(paymentMethod.card);
+
+  return res.json({
+    paymentMethod
+  })
+})
+
+// 결제 컨펌 API
+// PaymentIntentId랑 PaymentMethodId를 받아서 컨펌됨. 
+// 정상 동작하면 아래 콘솔에서 status: succeed 확인 가능.
+app.post('/confirm-payment-intent', async(req, res) => {
+  const { paymentIntentId: piid, paymentMethod: pmid } = req.body;
+
+  console.log(piid, pmid)
+
+  const stripe = new Stripe(stripeSecretKey, {
+    apiVersion: '2023-10-16',
+    typescript: false,
+  });
+
+  const paymentIntent = await stripe.paymentIntents.confirm(
+    piid,
+    {
+      payment_method: pmid,
+    }
+  );
+
+  console.log("paymentIntent Confirmed", paymentIntent);
+
+  return res.json({
+    paymentIntent
+  })
+})
+
+app.listen(8080, () => console.log(`Node server listening on port ${8080}!`));
